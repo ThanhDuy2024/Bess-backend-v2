ca40c3020427ca96397a3dfa31eb4d5748327eed
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2026-06-02 08:22:37 +0700

    [Update] fix

--- iot_core.js
+++ iot_core.js
@@ -9,7 +9,7 @@ const PORT = 3001;
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-let host = ["http://127.0.0.1", "http://localhost", "http://192.168.1.4:8000", "http://bess2.local:8000", "http://bess2.local"];
+let host = ["http://127.0.0.1", "http://localhost", process.env.IP_HOST_PORT, process.env.IP_HOST];
 
 app.use(
   cors({
