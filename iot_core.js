3b7995ef6165ddaf7f85c9f8606c60058bddcb96
kubdev36 <kub336655@gmail.com>
2026-06-02 08:18:19 +0700

    update api get + update user

--- iot_core.js
+++ iot_core.js
@@ -9,7 +9,7 @@ const PORT = 3001;
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-let host = ["http://127.0.0.1", "http://localhost", "http://192.168.1.4:8000"];
+let host = ["http://127.0.0.1", "http://localhost", "http://172.31.8.126:8000", "http://bess.local"];
 
 app.use(
   cors({
--- routes/data.js
+++ routes/data.js
@@ -21,4 +21,36 @@ router.post("/getModbusTemp", async (req, res)=>{
   }
 });
 
+router.post("/getAllUser", async (req, res)=>{
+  let temp = await apiData.funcTable("func_getuser", `()`);
+  if(temp.status){
+    res.status(200).json({
+      status:true,
+      data: temp.data
+    });
+  }
+  else{
+    res.status(200).json({
+      status: false,
+      mess: "DB Err"
+    });
+  }
+});
+
+router.post("/updateUser", async (req, res) => {
+  let temp = await apiData.funcJSON(
+    "func_updateuser",
+    `(
+      '${req.body.action}', ${req.body.id || 0}, '${req.body.name || ""}', '${req.body.email || ""}', '${req.body.password || ""}', '${req.body.username || ""}', '${req.body.role || ""}', '${req.body.status || ""}'
+    )`
+  );
+
+  if (temp.status) {
+    res.status(200).json(temp);
+  } else {
+    res.status(200).json(temp);
+  }
+});
+
+
 module.exports = router;
